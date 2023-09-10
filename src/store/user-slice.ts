afd6179a0931389ab55fa191bf11293421c9eb31
MacBook <macbook@MacBooks-MacBook-Air.local>
2023-09-10 16:22:24 +0300

    userslice for AuthForm

---
+++ src/store/user-slice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialUserState = { email: null }
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: initialUserState,
+    reducers: {
+        login(state, action) {
+            if (action.payload?.error) {
+                state = initialUserState;
+            } else {
+                state.email = action.payload;
+            }
+        }
+    }
+})
+export const userActions = userSlice.actions
+export default userSlice.reducer
